dd09f422079e4f76694b55f874eafc311a6d7a51
pot-code <136290354@qq.com>
2025-02-20 11:44:26 +0800

    feat: native locale

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { lightTheme, GlobalThemeOverrides } from 'naive-ui'
+import { lightTheme, dateZhCN, zhCN, GlobalThemeOverrides } from 'naive-ui'
 
 const themeOverrides: GlobalThemeOverrides = {}
 </script>
 
 <template>
-  <n-config-provider :theme="lightTheme" :theme-overrides="themeOverrides">
+  <n-config-provider :theme="lightTheme" :locale="zhCN" :date-locale="dateZhCN" :theme-overrides="themeOverrides">
     <router-view />
   </n-config-provider>
 </template>
